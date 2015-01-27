73fa6d48e9be4a11253b302df57513d50a2fad42
Janne Nikula <janne.nikula@applifier.com>
2015-01-27 16:34:36 +0200

    Bump version number

--- ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
+++ ios/UnityAds/UnityAdsProperties/UnityAdsProperties.m
@@ -12,7 +12,7 @@
 #import "../UnityAdsDevice/UnityAdsDevice.h"
 #import "UnityAdsCacheManager.h"
 
-NSString * const kUnityAdsVersion = @"1310";
+NSString * const kUnityAdsVersion = @"1450";
 
 @implementation UnityAdsProperties
 
